ca0ea88e5d32f08942cc0a1f98a1644437a7c064
Joseph Kassouf <jkassouf@upei.ca>
2022-11-17 16:56:23 -0400

    Created Expense Tracking with React

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState("2020");
+  const filterChangeHandler = (selectedYer) => {
+    setFilterYear(selectedYer);
+  };
+
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filterExpenses} />
+        <ExpensesList items={filterExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
